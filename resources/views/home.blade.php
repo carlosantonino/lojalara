e93f94e735a71230e066776e8808a0f72d21e209
Carlos Antonino <carlosantonino.cs@gmail.com>
2022-05-19 19:54:53 -0300

    craindo pagina home

---
+++ resources/views/home.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Myshop - Ecommerce</title>
+</head>
+<body>
+    <nav class="navbar navbar-light navbar-expand-md bg-light pl-5 pr-5 mb-5">
+        <a href="#" class="navbar-brand">Myshop</a>
+        <div class="collapse navbar-collapse">
+            <div class="navbar-nav">
+                <a href="#">Home</a>
+                <a href="#">Categorias</a>
+                <a href="#">Cadastrar</a>
+            </div>
+        </div>
+    </nav>
+
+</body>
+</html>
