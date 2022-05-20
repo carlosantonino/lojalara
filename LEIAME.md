fe49781f652df5b717cd77dc58feac85f335ebd3
Carlos Antonino <carlosantonino.cs@gmail.com>
2022-05-20 12:00:24 -0300

    auth

---
+++ LEIAME.md
@@ -0,0 +1,3 @@
+## https://youtu.be/lU6YyCDdACA?list=PL2Dw5PtrD32ywkdxHOu3hG4nLDGNQ1ojo&t=934
+
+Onde paramos
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,22 +1,23 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Myshop - Ecommerce</title>
-</head>
-<body>
-    <nav class="navbar navbar-light navbar-expand-md bg-light pl-5 pr-5 mb-5">
-        <a href="#" class="navbar-brand">Myshop</a>
-        <div class="collapse navbar-collapse">
-            <div class="navbar-nav">
-                <a href="#">Home</a>
-                <a href="#">Categorias</a>
-                <a href="#">Cadastrar</a>
+@extends('layouts.admin')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Dashboard') }}</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    {{ __('Seja bem vindo !') }}
+                </div>
             </div>
         </div>
-    </nav>
-
-</body>
-</html>
+    </div>
+</div>
+@endsection
